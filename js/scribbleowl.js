e3ce024acebd8b00bf2db33f62bbda97e2bb9cdb
Jessica Billings <jessica@meteorsolutions.com>
2014-02-03 18:46:44 -0800

    Added portfolio content

--- js/scribbleowl.js
+++ js/scribbleowl.js
@@ -10,5 +10,24 @@
 		$(".dial").each(function (i) {
 			$(this).knob(dialParams);
 		});
+		
+		/* Colorbox */
+		$(".admindash").colorbox({rel: "admindash", scalePhotos: 'true', maxWidth: '80%'});
+		$(".xbox").colorbox({rel: "xbox", scalePhotos: 'true', maxWidth: '80%'});
+		$(".dash").colorbox({rel: "dash", scalePhotos: 'true', maxWidth: '80%'});
+		$(".liketool").colorbox({rel: "liketool", scalePhotos: 'true', maxWidth: '80%'});
+
+		$("#xbox-link").click(function() {
+			$("#xboxfirst").click();
+		});
+		$("#admin-link").click(function() {
+			$("#adminfirst").click();
+		});
+		$("#dash-link").click(function() {
+			$("#dashfirst").click();
+		});
+		$("#liketool-link").click(function() {
+			$("#likefirst").click();
+		});
 	});
 })(window, document, jQuery);
